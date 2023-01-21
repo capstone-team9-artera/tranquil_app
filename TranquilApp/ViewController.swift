54b14a2abf7c4e7f8e1b7fc30a674f53d5205546
hdinh77 <heatherdinh@ucsb.edu>
2023-01-20 21:43:02 -0800

    initial commit

---
+++ TranquilApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TranquilApp
+//
+//  Created by Heather Dinh on 1/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
