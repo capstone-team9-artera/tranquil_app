b9ae195b731093771ea8e7e51d70355c045e7eb9
Archana Neupane <archananeupane@umail.ucsb.edu>
2023-01-25 14:46:28 -0800

    new journal components

---
+++ TranquilApp/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TranquilApp
+//
+//  Created by archana neupane on 1/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeView()
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
