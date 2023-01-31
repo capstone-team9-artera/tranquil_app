475407d0c0b8f177f33fee8f5305c77e71110f74
hdinh77 <heatherdinh@ucsb.edu>
2023-01-31 15:27:18 -0800

    ui updates, fixing some views from merge, still need journal

--- TranquilApp/ViewControllers/JournalViewController.swift
+++ TranquilApp/ViewControllers/JournalViewController.swift
@@ -9,26 +9,18 @@ import SwiftUI
 
 class JournalViewController: UIViewController {
     private let myView = UIView(frame: CGRect())
-    private let textField = UITextField(frame: CGRect(x: 100, y: 450, width: 200, height: 21))
-    private let journalText = UILabel(frame: CGRect(x: 100, y: 400, width: 200, height: 21))
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        myView.backgroundColor = .systemCyan
+        myView.backgroundColor = .white
         view = myView
-        title = "Journal Entries"
+        title = "Journal"
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "< Home", style: .plain, target: self, action: #selector(dismissSelf))
-
-
-        view.backgroundColor = .white
-        view.addSubview(journalText)
-        journalText.text = "Journal Entry"
-        journalText.textColor = .black
-        journalText.textAlignment = .center
-        
-        textField.borderStyle = .roundedRect
-        textField.placeholder = "Entry here"
-        view.addSubview(textField)
+        let controller = UIHostingController(rootView: HomeView())
+        controller.view.frame = CGRect(x: 0, y: 100, width: 400, height: 700)
+        controller.didMove(toParent: self)
+        controller.modalPresentationStyle = .fullScreen
+        view.addSubview(controller.view)
     }
     
     @objc private func dismissSelf() {
