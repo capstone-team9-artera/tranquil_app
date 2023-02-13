50b964e7aabe135736a3e6eeeab1c2e9f5f823e4
hdinh77 <heatherdinh@ucsb.edu>
2023-02-13 14:29:24 -0800

    breathing view exercises added:

--- TranquilApp/Pages/SessionViewController.swift
+++ TranquilApp/Pages/SessionViewController.swift
@@ -8,32 +8,62 @@ import SwiftUI
 
 class SessionViewController: UIViewController {
     var name: String = "Default"
-//    let description: String = "Default description"
+    var prompt: String = "Default description"
     var gifName: String = "sunsetocean"
     
     private let label = UILabel()
+    private let promptLabel = UILabel()
     private let myView = UIView(frame: CGRect())
-
+    private let animation = AnimationView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         myView.backgroundColor = .white
         view = myView
         view.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
-
+//        view.addSubview(animation)
+        
         label.textAlignment = .center
         label.text = name
         label.textColor = .white
         label.frame = CGRect(x: 25, y: 200, width: 350, height: 52)
         label.font = .systemFont(ofSize: 30, weight: UIFont.Weight(rawValue: 5))
+        
+        promptLabel.textAlignment = .center
+        promptLabel.text = prompt
+        promptLabel.textColor = .white
+        promptLabel.frame = CGRect(x: 25, y: 300, width: 350, height: 52)
+        promptLabel.font = .systemFont(ofSize: 18, weight: UIFont.Weight(rawValue: 3))
 
         let myGif = UIImage.gifImageWithName(gifName)
         let imageView = UIImageView(image: myGif)
         imageView.frame = CGRect(x: 20, y: 20, width: 350, height: 700)
         imageView.layer.cornerRadius = 10
         imageView.inputViewController?.modalPresentationStyle = .fullScreen
+        
         view.addSubview(imageView)
         view.addSubview(label)
+        view.addSubview(promptLabel)
         view.layer.cornerRadius = 10
+        
+        
+        let gesture = UITapGestureRecognizer(target: self, action:  #selector(self.goToView(_:)))
+        view.addGestureRecognizer(gesture)
+    }
+    
+    @objc private func goToView(_ sender:UITapGestureRecognizer) {
+//        print(name)
+//        let rootVC = HistoryViewController()
+//        let navVC = UINavigationController(rootViewController: HistoryView())
+//        navVC.modalPresentationStyle = .fullScreen
+//        present(navVC, animated: false)
+        let controller = UIHostingController(rootView: AnimationView(name: name))
+//        parent.view.addSubview(controller.view)
+        controller.didMove(toParent: self)
+//        controller.modalPresentationStyle = .fullScreen
+        present(controller, animated: true)
+
+
     }
     
     @objc private func dismissSelf() {
