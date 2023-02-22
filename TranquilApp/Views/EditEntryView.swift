2629c710e94292e875a5dba9cded461e46e1269c
Archana Neupane <archananeupane@umail.ucsb.edu>
2023-02-22 10:48:43 -0800

    fixed edit entry

--- TranquilApp/Views/EditEntryView.swift
+++ TranquilApp/Views/EditEntryView.swift
@@ -7,46 +7,74 @@
 
 import SwiftUI
 
-struct EntryRowView: View {
-    var entry: Entry
+
+struct EditEntryView: View {
+    @ObservedObject var entryModelController: EntryModelController
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State var text: String? = nil
+    @State private var emotionState: EmotionState = .happy
+    @State private var moodColor: MoodColor = .happyColor
+    @State private var happyIsSelected = false
+    @State private var mehIsSelected = false
+    @State private var sadIsSelected = false
+    @State private var counterLabel = ""
     
     var body: some View {
-  ZStack {
-  
-  Rectangle().fill(Color(UIColor.systemBackground)).cornerRadius(10).shadow(color: .gray, radius: 5, x: 1, y: 1)
-        HStack {
-            VStack {
-            Text(entry.monthString)
-            Text("\(entry.dayAsInt)")
-            
+        VStack(spacing: 30) {
+            Text("Add an Entry").font(.largeTitle)
+            HStack {
+                Button(action: {
+                    self.emotionState = .happy
+                    self.moodColor = .happyColor
+                    self.happyIsSelected = true
+                    self.mehIsSelected = false
+                    self.sadIsSelected = false
+                }) {
+                    Image("happy").resizable().frame(width: 50, height: 50).foregroundColor(.green).background(happyIsSelected ? Color.yellow : Color.clear).clipShape(Circle())
+                }
+                
+                Button(action: {
+                    self.emotionState = .meh
+                    self.moodColor = .mehColor
+                    self.mehIsSelected = true
+                    self.happyIsSelected = false
+                    self.sadIsSelected = false
+                }) {
+                    Image("meh").resizable().frame(width: 50, height: 50).foregroundColor(.gray).background(mehIsSelected ? Color.yellow : Color.clear).clipShape(Circle())
+                }
+                
+                Button(action: {
+                    self.emotionState = .sad
+                    self.moodColor = .sadColor
+                    self.sadIsSelected = true
+                    self.happyIsSelected = false
+                    self.mehIsSelected = false
+                }) {
+                    Image("sad").resizable().frame(width: 50, height: 50).foregroundColor(.red).background(sadIsSelected ? Color.yellow : Color.clear).clipShape(Circle())
+                }
             }
-            Text(entry.comment ?? "No comment made.").font(.title).bold()
-            
+            ZStack(alignment: .bottomTrailing) {
+            MultiLineTextField(txt: $text, counterLabel: $counterLabel).frame(height: 450).cornerRadius(20)
+//                Text("Remaining: \(counterLabel)").font(.footnote).foregroundColor(.gray).padding([.bottom, .trailing], 8)
+        }
+            Button(action: {
+                    self.entryModelController.createEntry(emotion: Emotion(state: self.emotionState, color: self.moodColor), comment: self.text, date: Date())
+                    
+                self.presentationMode.wrappedValue.dismiss()
+            }) {
+                Text("Edit Entry").bold().frame(width: UIScreen.main.bounds.width - 30, height: 50).background(Color.blue).foregroundColor(.white).cornerRadius(10)
+            }
             Spacer()
-            
-            entryImage()
-           
-            }.foregroundColor(entry.emotion.moodColor).padding()
-        }
+        }.padding()
     }
-    
-    func entryImage() -> some View {
-        var imageName = "none"
-        
-        switch entry.emotion.state {
-        case .happy:
-            imageName = "happy"
-        case .meh:
-            imageName = "meh"
-        case .sad:
-            imageName = "sad"
-        }
-        return Image(imageName).resizable().frame(width: 20, height: 20)
-    }
+
 }
 
-struct MoodRowView_Previews: PreviewProvider {
+struct AddEntryView_Previews: PreviewProvider {
+    
     static var previews: some View {
-        EntryRowView(entry: Entry(emotion: Emotion(state: .happy, color: .happyColor), comment: "Test", date: Date()))
+        
+        AddEntryView(entryModelController: EntryModelController(), text: "text")
     }
 }
