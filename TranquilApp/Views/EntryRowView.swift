3f450330de1fe3c078b3253ed4907578e9582fc2
Archana Neupane <archananeupane@umail.ucsb.edu>
2023-02-25 12:59:05 -0800

    fixed journal edit

--- TranquilApp/Views/EntryRowView.swift
+++ TranquilApp/Views/EntryRowView.swift
@@ -7,54 +7,6 @@
 
 import SwiftUI
 
-//struct EntryRowView: View {
-//    @ObservedObject var entryModelController: EntryModelController
-//    var entry: Entry
-//
-//    var body: some View {
-//  ZStack {
-//
-//  Rectangle().fill(Color(UIColor.systemBackground)).cornerRadius(10).shadow(color: .gray, radius: 5, x: 1, y: 1)
-//        HStack {
-//            VStack {
-//            Text(entry.monthString)
-//            Text("\(entry.dayAsInt)")
-//            }
-//
-//
-//            Text(entry.comment ?? "No comment made.").font(.title).bold()
-//
-////                Image(systemName: "pencil")
-////                    .foregroundColor(.blue)
-////
-//            Spacer()
-//
-//            entryImage()
-//
-//            }.foregroundColor(entry.emotion.moodColor).padding()
-//        }
-//    }
-//
-//    func entryImage() -> some View {
-//        var imageName = "none"
-//
-//        switch entry.emotion.state {
-//        case .happy:
-//            imageName = "happy"
-//        case .meh:
-//            imageName = "meh"
-//        case .sad:
-//            imageName = "sad"
-//        }
-//        return Image(imageName).resizable().frame(width: 20, height: 20)
-//    }
-//}
-//struct EntryRowView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        EntryRowView(entryModelController: EntryModelController(), entry: Entry(emotion: Emotion(state: .happy, color: .happyColor), comment: "Test", date: Date()))
-//    }
-//}
-
 struct EntryRowView: View {
     @ObservedObject var entryModelController: EntryModelController
     var entry: Entry
@@ -78,21 +30,28 @@ struct EntryRowView: View {
                     Text(entry.monthString)
                     Text("\(entry.dayAsInt)")
                 }
-                
-                //update entry comment
-                TextField("Enter comment", text: $updatedComment, onCommit: {
-                    entryModelController.updateEntryComment(entry: entry, comment: updatedComment)
-                })
-                .font(.title)
-                .bold()
 
+                //update entry comment
+                TextEditor(text: $updatedComment)
+                    .font(.system(size: 16))
+                    .bold()
+                    .frame(minHeight: 50)
+                    .lineSpacing(10)
+                    .onAppear {
+                        UITextView.appearance().backgroundColor = .clear
+                    }
+                    .onChange(of: updatedComment) { value in
+                        entryModelController.updateEntryComment(entry: entry, comment: updatedComment)
+                    }
+                
                 Spacer()
                 entryImage()
                 
                 Image(systemName: "pencil")
                     .foregroundColor(.blue)
                     .onTapGesture {
-                        updatedComment = entry.comment ?? ""
+                        entryModelController.updateEntryComment(entry: entry, comment: updatedComment)
+                        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to:nil, from:nil, for:nil)
                     }
             }
             .foregroundColor(entry.emotion.moodColor)
@@ -114,6 +73,7 @@ struct EntryRowView: View {
         return Image(imageName).resizable().frame(width: 20, height: 20)
     }
 }
+
 struct EntryRowView_Previews: PreviewProvider {
     static var previews: some View {
         EntryRowView(entryModelController: EntryModelController(), entry: Entry(emotion: Emotion(state: .happy, color: .happyColor), comment: "Test", date: Date()))
